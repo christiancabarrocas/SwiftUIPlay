ee458595369248b2cce1c702f5c25ca32039b45a
christiancabarrocas <christiancabarrocas@gmail.com>
2020-10-08 11:03:55 +0200

    Initial Commit

---
+++ SwiftUIPlay/SwiftUIPlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPlayApp.swift
+//  SwiftUIPlay
+//
+//  Created by Shootr on 08/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
